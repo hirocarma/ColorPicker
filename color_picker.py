37fddce15de016d17729f95ecb50d9e7485334b7
hiro <hiro@carmapenguin.net>
2023-09-06 22:43:56 +0900

    Add HSV info(complementary color & opposite color)

--- color_picker.py
+++ color_picker.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 import colorsys
 import math
+from PIL import Image
 
 drawing = False
 
@@ -43,14 +44,15 @@ def printColor(event,x,y,flags,param):
             c_r = val - r
             c_g = val - g
             c_b = val - b
+
             #opposite color
             r_r = 255 - r
             r_g = 255 - g
             r_b = 255 - b
-            
+
             height, width = rec_img.shape[:2]
-            pick_height = max(240, int(height*1.1))
-            pick_width = max(360, int(width*1.1))
+            pick_height = max(240, int(height*1.2))
+            pick_width = max(360, int(width*1.2))
             pick = np.full((pick_height, pick_width, 3), (b, g, r), dtype=np.uint8)
             
             pick[0:height, 0:width] = rec_img
@@ -66,30 +68,63 @@ def printColor(event,x,y,flags,param):
                     cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1, cv2.LINE_AA)
 
             cv2.rectangle(pick,
-              pt1=(200, 120),
-              pt2=(360, 180),
+              pt1=(200, 60),
+              pt2=(360, 140),
               color=(c_b, c_g, c_r),
               thickness=-1)
+
+            c_lab_img = cv2.cvtColor(pick, cv2.COLOR_RGB2Lab)
+            c_L_ast = int(np.mean(pick[:,:,0]))
+            c_a_ast = int(np.mean(pick[:,:,1]))
+            c_b_ast = int(np.mean(pick[:,:,2]))
+            c_c_ast = int(math.sqrt(a_ast**2+b_ast**2))
+
+            c_hsv_img = cv2.cvtColor(pick,cv2.COLOR_RGB2HSV)
+            c_h = int(np.mean(c_hsv_img[:,:,0]))
+            c_s = int(np.mean(c_hsv_img[:,:,1]))
+            c_v = int(np.mean(c_hsv_img[:,:,2]))
+
             c_txt0 = 'Complementary Color'
-            cv2.putText(pick, c_txt0, (180, 140), \
+            cv2.putText(pick, c_txt0, (180, 80), \
                     cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1, cv2.LINE_AA)
             c_txt1 = 'RGB:' + str(c_r) + ',' + str (c_g) + \
                 ',' + str(c_b)
-            cv2.putText(pick, c_txt1, (200, 160), \
+            cv2.putText(pick, c_txt1, (200, 100), \
+                    cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1, cv2.LINE_AA)
+            c_txt2 = 'HSV:' + str(c_h) + ',' + str (c_s) + \
+                ',' + str(c_v)
+            cv2.putText(pick, c_txt2, (200, 120), \
                     cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1, cv2.LINE_AA)
 
             cv2.rectangle(pick,
-              pt1=(200, 180),
+              pt1=(200, 160),
               pt2=(360, 360),
               color=(r_b, r_g, r_r),
               thickness=-1)
+
+            r_lab_img = cv2.cvtColor(pick, cv2.COLOR_RGB2Lab)
+            r_L_ast = int(np.mean(pick[:,:,0]))
+            r_a_ast = int(np.mean(pick[:,:,1]))
+            r_b_ast = int(np.mean(pick[:,:,2]))
+            r_c_ast = int(math.sqrt(a_ast**2+b_ast**2))
+
+            r_hsv_img = cv2.cvtColor(pick,cv2.COLOR_RGB2HSV)
+            r_h = int(np.mean(r_hsv_img[:,:,0]))
+            r_s = int(np.mean(r_hsv_img[:,:,1]))
+            r_v = int(np.mean(r_hsv_img[:,:,2]))
+
             r_txt0 = 'Opposite Color'
-            cv2.putText(pick, r_txt0, (180, 200), \
+            cv2.putText(pick, r_txt0, (180, 180), \
                     cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1, cv2.LINE_AA)
-            r_txt1 = 'RGB:' + str(c_r) + ',' + str (c_g) + \
-                ',' + str(c_b)
-            cv2.putText(pick, r_txt1, (200, 220), \
+            r_txt1 = 'RGB:' + str(r_r) + ',' + str (r_g) + \
+                ',' + str(r_b)
+            cv2.putText(pick, r_txt1, (200, 200), \
                     cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1, cv2.LINE_AA)
+            r_txt2 = 'HSV:' + str(r_h) + ',' + str (r_s) + \
+                ',' + str(r_v)
+            cv2.putText(pick, r_txt2, (200, 220), \
+                    cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1, cv2.LINE_AA)
+
             
             print(txt0 + txt1 + txt2 + '| ' + c_txt0 + c_txt1)
 
